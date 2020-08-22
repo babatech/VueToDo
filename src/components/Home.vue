551f7bd6b679065e79d3ff3a4e7af87c2ac64c15
Shahab Shakoor <shahabi1209@gmail.com>
2020-08-22 16:13:32 +0200

    index component changed

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      list: []
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>>
+
+</style>
