be25328ae7a62186c244772ef062f5cc7203c1ea
Shahab Shakoor <shahabi1209@gmail.com>
2020-08-22 17:29:46 +0200

    Integration of header component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,30 @@
 <template>
-  
+  <div>
+    <vue-header></vue-header>
+    <div class="container">
+      <h2>List of tasks</h2>
+    </div>
+  </div>
 </template>
 
 <script>
+import vueHeader from './header'
 export default {
   name: 'Home',
+  components: { vueHeader },
   data () {
     return {
       list: []
     }
   },
-  mounted() {
-  },
-  methods: {
-    
-  },
+  mounted () {},
+  methods: {}
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style>
+.container{
+  background: #f9f9f9;
+  min-height: 100%;
+}
 </style>
