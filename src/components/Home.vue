c8d27b7d695ff1db6ace4279e69095e22e1146d1
Shahab Shakoor <shahabi1209@gmail.com>
2020-08-24 23:49:27 +0200

    Hide complete task filter on List component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,12 @@
     <vue-header></vue-header>
     <div class="container">
       <h2 class="home-title">Welcome to To Do App</h2>
-      <task-list></task-list>
+      <br>
+      <h3>Today tasks</h3>
+      <task-list :todayOnly="true"></task-list>
+      <hr>
+      <h3>Coming tasks</h3>
+      <task-list ></task-list>
     </div>
   </div>
 </template>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,59 +1,80 @@
 <template>
   <div>
-    <div>
-      <form class="form-inline"  @submit="search">
-        <div class="form-row align-items-center">
-          <div class="col-auto">
-            <label class="sr-only" for="inlineFormInputGroup">Search tasks</label>
-            <div class="input-group mb-2">
-              <div class="input-group-prepend">
-                <div class="input-group-text">Search tasks</div>
+    <div v-if="tasks && tasks.length>0">
+      <div class="row">
+        <form class="col-md-8 form-inline"  @submit="search">
+          <div class="form-row align-items-center">
+            <div class="col-auto">
+              <label class="sr-only" for="inlineFormInputGroup">Search tasks</label>
+              <div class="input-group mb-2">
+                <div class="input-group-prepend">
+                  <div class="input-group-text">Search tasks</div>
+                </div>
+                <input type="text" v-model="searchText" class="form-control" id="inlineFormInputGroup" placeholder="Buy">
               </div>
-              <input type="text" v-model="searchText" class="form-control" id="inlineFormInputGroup" placeholder="Buy">
+            </div>
+            <div class="col-auto">
+              <div class="form-check mb-2">
+                <input class="form-check-input" v-model="searchType"  type="checkbox" id="autoSizingCheck">
+                <label class="form-check-label" for="autoSizingCheck">
+                  Search title only
+                </label>
+              </div>
+            </div>
+            <div class="col-auto">
+              <button type="submit" class="btn btn-primary mb-2">Search</button>
             </div>
           </div>
-          <div class="col-auto">
-            <div class="form-check mb-2">
-              <input class="form-check-input" v-model="searchType"  type="checkbox" id="autoSizingCheck">
-              <label class="form-check-label" for="autoSizingCheck">
-                Search title only
-              </label>
-            </div>
+        </form>
+          <div class="col-md-4 ta-r">
+            <input class="form-check-input" v-model="hideCompleteTask"  type="checkbox" id="hideCompleteTaskCheck">
+            <label class="form-check-label" for="hideCompleteTaskCheck">
+              Hide complete task
+            </label>
           </div>
-          <div class="col-auto">
-            <button type="submit" class="btn btn-primary mb-2">Search</button>
-          </div>
-        </div>
-      </form>
+      </div>
+      <table class="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col" class="task-title">Task title</th>
+            <th scope="col">Due date</th>
+            <th scope="col">State</th>
+            <th scope="col"><font-awesome-icon :icon="['fas', 'tools']" /></th>
+          </tr>
+        </thead>
+        <tbody >
+          <template v-for="(task, index) in tasks ">
+            <tr class="task" v-show="!hideCompleteTask || (hideCompleteTask && !task.state) " :key="index" :class="task.state ? 'complete' : 'pending'">
+              <th scope="row">{{index + 1}}</th>
+              <td><a class="task-text" title="Click here to open task" v-bind:href="'view/'+task.id">{{task.title}}</a></td>
+              <td><a class="task-text" title="Click here to open task" v-bind:href="'view/'+task.id">{{task.datetime}}</a></td>
+              <td>
+                <a class="mark-btn" @click="changeState(task)" :title="'Click hee to mark this task as ' + (task.state ? 'pending' : 'complete')">
+                <font-awesome-icon :icon="['fas', task.state ? 'check-square' : 'clipboard-list']" /> {{task.state ? 'Complete': 'Pending'}}
+                </a>
+              </td>
+              <td>
+                <a title="Click here to open task" v-bind:href="'view/'+task.id"><font-awesome-icon :icon="['fas', 'book-open']" /></a>
+                <a title="Click here to edit task" v-bind:href="'edit/'+task.id"><font-awesome-icon :icon="['fas', 'edit']" /></a>
+                <a title="Click here to delete task" @click="deleteTask(task.id)"><font-awesome-icon :icon="['fas', 'trash-alt']" /></a>
+              </td>
+            </tr>
+
+          </template>
+        </tbody>
+      </table>
     </div>
-    <table class="table table-dark">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col" class="task-title">Task title</th>
-          <th scope="col">Due date</th>
-          <th scope="col">State</th>
-          <th scope="col"><font-awesome-icon :icon="['fas', 'tools']" /></th>
-        </tr>
-      </thead>
-      <tbody v-if="tasks.length>0">
-        <tr class="task" v-for="(task, index) in tasks " :key="index" :class="task.state ? 'complete' : 'pending'">
-          <th scope="row">{{index + 1}}</th>
-          <td><a class="task-text" title="Click here to open task" v-bind:href="'view/'+task.id">{{task.title}}</a></td>
-          <td><a class="task-text" title="Click here to open task" v-bind:href="'view/'+task.id">{{task.datetime}}</a></td>
-          <td>
-            <a class="mark-btn" @click="changeState(task)" :title="'Click hee to mark this task as ' + (task.state ? 'pending' : 'complete')">
-            <font-awesome-icon :icon="['fas', task.state ? 'check-square' : 'clipboard-list']" /> {{task.state ? 'Complete': 'Pending'}}
-            </a>
-          </td>
-          <td>
-            <a title="Click here to open task" v-bind:href="'view/'+task.id"><font-awesome-icon :icon="['fas', 'book-open']" /></a>
-            <a title="Click here to edit task" v-bind:href="'edit/'+task.id"><font-awesome-icon :icon="['fas', 'edit']" /></a>
-            <a title="Click here to delete task" @click="deleteTask(task.id)"><font-awesome-icon :icon="['fas', 'trash-alt']" /></a>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div v-if="tasks && tasks.length===0">
+      <div class="alert alert-info" role="alert">
+        <span v-if="todayOnly">
+          No Task for today
+        </span>
+        <span v-if="!todayOnly">
+          No upcoming task
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -61,19 +82,29 @@
 import taskService from '@/services/task.service'
 export default {
   name: 'taskList',
-  props: [ 'tomorrow' ],
+  props: [ 'todayOnly' ],
   data () {
     return {
       tasks: [],
       searchText: '',
+      date: new Date(),
+      hideCompleteTask: false,
       searchType: true
     }
   },
   mounted () {
+    if (!this.todayOnly) {
+      this.date.setDate(this.date.getDate() + 1)
+    }
     this.getTasks()
   },
   methods: {
     getTasks (queryParam = {}) {
+      if (this.todayOnly) {
+        queryParam.datetime = this.date.toISOString().substring(0, 10)
+      } else {
+        queryParam.datetime_gte = this.date.toISOString().substring(0, 10)
+      }
       taskService.getAllTasks(queryParam).then(res => {
         this.tasks = res.data
       })
@@ -114,5 +145,8 @@ export default {
 .task a{
   cursor: pointer;
   color: white;
+}
+.ta-r{
+  text-align: right;
 }
 </style>
